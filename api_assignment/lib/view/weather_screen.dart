f2935c44e318c68ac57d715d8ea5bc37078dae2f
Arham Sarwar <arhamsarwar786@gmail.com>
2022-12-25 23:48:38 +0500

    refector

--- api_assignment/lib/view/weather_screen.dart
+++ api_assignment/lib/view/weather_screen.dart
@@ -12,131 +12,130 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
-
   WeatherModel? weatherData;
 
   getWeather() async {
-   var data = await ApiManager.fetchWeather("lahore");
-  weatherData = WeatherModel.fromJson(data);
-  //  weatherData = data;
-   setState(() {
-     
-   });
-   print(weatherData);
+    var data = await ApiManager.fetchWeather("lahore");
+    weatherData = WeatherModel.fromJson(data);
+    //  weatherData = data;
+    setState(() {});
+    print(weatherData);
   }
 
   @override
-  void initState() {  
+  void initState() {
     super.initState();
     getWeather();
   }
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Container(
       decoration: const BoxDecoration(
           image: DecorationImage(
               image: AssetImage("asset/images/sun.jpg"), fit: BoxFit.cover)),
       child: Scaffold(
-        floatingActionButton: FloatingActionButton(onPressed: (){
+        floatingActionButton: FloatingActionButton(onPressed: () {
           getWeather();
         }),
         backgroundColor: Colors.transparent,
-        body: weatherData == null ? CircularProgressIndicator.adaptive() : Padding(
-          padding: const EdgeInsets.only(top: 50),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text("${weatherData}"),
-              Text("${weatherData!.name}",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 60,
-                      fontWeight: FontWeight.bold)),
-              SizedBox(height: 20),
-              Text("Temperature ${weatherData!.main!.temp! - 273}",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold)),
-              SizedBox(height: 150),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      Text("humidity",
-                          style: TextStyle(
+        body: weatherData == null
+            ? CircularProgressIndicator.adaptive()
+            : Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("${weatherData}"),
+                    Text("${weatherData!.name}",
+                        style: TextStyle(
                             color: Colors.white,
-                          )),
-                      SizedBox(height: 5),
-                      Text("humidity",
-                          style: TextStyle(
+                            fontSize: 60,
+                            fontWeight: FontWeight.bold)),
+                    SizedBox(height: 20),
+                    Text("Temperature ${weatherData!.main!.temp! - 273}",
+                        style: TextStyle(
                             color: Colors.white,
-                          )),
-                    ],
-                  ),
-                  SizedBox(width: 10),
-                  Column(
-                    children: [
-                      Text("humidity",
-                          style: TextStyle(
-                            color: Colors.white,
-                          )),
-                      SizedBox(height: 5),
-                      Text("humidity",
-                          style: TextStyle(
-                            color: Colors.white,
-                          )),
-                    ],
-                  ),
-                  SizedBox(width: 10),
-                  Column(
-                    children: [
-                      Text("humidity",
-                          style: TextStyle(
-                            color: Colors.white,
-                          )),
-                      SizedBox(height: 5),
-                      Text("humidity",
-                          style: TextStyle(
-                            color: Colors.white,
-                          )),
-                    ],
-                  ),
-                  SizedBox(width: 10),
-                  Column(
-                    children: [
-                      Text("humidity",
-                          style: TextStyle(
-                            color: Colors.white,
-                          )),
-                      SizedBox(height: 5),
-                      Text("humidity",
-                          style: TextStyle(
-                            color: Colors.white,
-                          )),
-                    ],
-                  ),
-                  SizedBox(width: 10),
-                  Column(
-                    children: [
-                      Text("humidity",
-                          style: TextStyle(
-                            color: Colors.white,
-                          )),
-                      SizedBox(height: 5),
-                      Text("humidity",
-                          style: TextStyle(
-                            color: Colors.white,
-                          )),
-                    ],
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold)),
+                    SizedBox(height: 150),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Text("humidity",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )),
+                            SizedBox(height: 5),
+                            Text("humidity",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )),
+                          ],
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          children: [
+                            Text("humidity",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )),
+                            SizedBox(height: 5),
+                            Text("humidity",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )),
+                          ],
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          children: [
+                            Text("humidity",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )),
+                            SizedBox(height: 5),
+                            Text("humidity",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )),
+                          ],
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          children: [
+                            Text("humidity",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )),
+                            SizedBox(height: 5),
+                            Text("humidity",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )),
+                          ],
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          children: [
+                            Text("humidity",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )),
+                            SizedBox(height: 5),
+                            Text("humidity",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
       ),
     );
   }
